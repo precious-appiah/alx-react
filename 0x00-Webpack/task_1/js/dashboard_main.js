9144cad26cbea3a9ff3d980d1680f9ec4cf54bdb
Awura Precious <aaprecious99@gmail.com>
2024-10-28 22:15:42 +0000

    task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,12 +1,21 @@
 import $ from "jquery";
+import { functions } from "lodash";
 
 $("body").append(`<p>Holberton Dashboard</p>`);
 $("body").append(`<p>Dashboard data for the students</p>`);
-$("document").ready(()=>{
-    const button = $(<button>Click here to get started</button>)
-    $('body').append(button)
-})
+$("document").ready(() => {
+  const button = $("<button>Click here to get started</button>");
+
+  $("body").append(button);
+
+  var counter = 0;
+  const updateCounter = () => {
+    counter++;
+    $("#count").html(counter + " clicks on the button");
+  };
+
+  // Attach the click event using jQuery
+  button.on("click", updateCounter);
+});
 $("body").append(`<p id='count'></p>`);
 $("body").append(`<p>Copyright - Holberton School</p>`);
-
-
