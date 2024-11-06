5255ff42b7ddd7d1e52e5e3efae9aed7fdb8c940
Awura Precious <aaprecious99@gmail.com>
2024-11-06 15:51:39 +0000

    removing multiple

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Login from "./Login/Login";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-import Notifications from "./Notifications";
+import Notifications from "./Notifications/Notifications";
 
 
 
