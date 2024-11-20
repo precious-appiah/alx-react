6584769d0497570b82921a708876fc64baa6d9c3
Awura Precious <aaprecious99@gmail.com>
2024-11-20 21:38:39 +0000

    task 0 fix 3

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,13 +1,5 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject (obj) {
-  console.log(fromJS(obj));
   return fromJS(obj);
 }
-
-getImmutableObject({
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-});
