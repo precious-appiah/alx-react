19ecda56be8755f2e21781f3a8d86a69d9c32654
Awura Precious <aaprecious99@gmail.com>
2024-11-21 07:53:14 +0000

    task 5 fix

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,8 +1,8 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 export function concatElements (page1, page2) {
   return List(page1.concat(page2));
 }
 
 export function mergeElements (page1, page2) {
-  return List(page1.merge(page2));
+  return Map(page1.merge(page2));
 }
