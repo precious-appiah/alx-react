83dde95a97822b5d96c566f06f077eae5b7026ce
Awura Precious <aaprecious99@gmail.com>
2024-11-21 08:14:46 +0000

    task 5 & 6 fix

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,8 +1,8 @@
 import { List, Map } from 'immutable';
 export function concatElements (page1, page2) {
-  return List(page1.concat(page2));
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements (page1, page2) {
-  return Map(page1.merge(page2));
+  return Map(page1).merge(Map(page2));
 }
--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,4 +1,4 @@
 import {Map} from 'immutable'
 export function mergeDeeplyElements(page1, page2){
-    return Map(page1.deepMerge(page2))
+    return Map(page1).deepMerge(Map(page2))
 }
